f873108ecaaad1b5409eb077f21759f445a04c14
Brandon Supinski <66806979+bsupinski@users.noreply.github.com>
2024-05-28 16:30:09 -0400

    added delete functionality

--- app.py
+++ app.py
@@ -53,32 +53,22 @@ def editproject(id):
         date = request.form['date']
         project.skills_practiced = request.form['skills']
         project.project_url = request.form['github']
+        db.session.commit()
+        return redirect(url_for('index'))
     return render_template('editprojectform.html', project=project, date=date)
 
 
 @app.route("/projects/<id>/delete")
-def deleteproject(id):
-    return render_template('index.html')
+def delete_project(id):
+    project = Project.query.get_or_404(id)
+    db.session.delete(project)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html', msg=error), 404
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
